529aad21712fa66ad8f4c53d8bc74d3496b82cad
Jeverson Santos <jeverson.apsantos@gmail.com>
2024-11-06 20:59:25 -0300

    Operacoes adicionadas

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -4,10 +4,12 @@ export const InputContainer = styled.div`
     width: 100%;
     height: 75px;
     background-color: #AAAAFF;
+    
 
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    
 
     font-size: 24px;
     font-family: 'Roboto';
@@ -18,6 +20,7 @@ export const InputContainer = styled.div`
         height: 75px;
         background-color: #AAAAFF;
         border: 0;
+        border-radius: 15px;
         display: flex;
         flex-direction: column;
         align-items: flex-end;
